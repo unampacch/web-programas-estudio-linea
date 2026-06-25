41640c179010c4bec31b2a284e97e813bbe0988c
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2026-06-24 21:19:38 -0600

    Implement service delay notification modal and enhance filtering functionality

--- js/functions.js
+++ js/functions.js
@@ -43,51 +43,66 @@
 window.onload = function() {
   var Shuffle = window.Shuffle;
   var element = document.querySelector('.shuffle-container');
+  var firstFilter = document.querySelector('.shuffle-filter li:first-child');
 
-  // Add "active" class to the first li on page load
-  document.querySelector('.shuffle-filter li:first-child').classList.add('active');
+  if (!Shuffle || !element || !firstFilter || typeof $ === 'undefined') {
+    return;
+  }
+
+  firstFilter.classList.add('active');
 
   var shuffleInstance = new Shuffle(element, {
-    itemSelector: 'div', // Adjust selector if using different elements
-    // Key modification: Initially filter for the first active group
+    itemSelector: 'div',
     group: document.querySelector('.shuffle-filter li.active').dataset.target
   });
 
   $('.shuffle-filter li').on('click', function(e) {
     e.preventDefault();
 
-    // Remove "active" class from all li elements
     $('.shuffle-filter li').removeClass('active');
-
-    // Add "active" class to the clicked li element
     $(this).addClass('active');
 
-    // Get the data-target value as a string array for filtering
     var keyword = $(this).attr('data-target').split(',');
 
-    // Apply the filtering to Shuffle.js
     shuffleInstance.filter(keyword);
   });
 };
 
-
-    new PureCounter({
-      selector: '.purecounter',
-      duration: 2,
-      delay: 10,
-      repeat: true,
-      decimals: 0,
-      legacy: true,
-      filesizing: false,
-      currency: false,
-      separator: false,
+if (typeof PureCounter !== 'undefined') {
+  new PureCounter({
+    selector: '.purecounter',
+    duration: 2,
+    delay: 10,
+    repeat: true,
+    decimals: 0,
+    legacy: true,
+    filesizing: false,
+    currency: false,
+    separator: false,
   });
+}
 
-  wow = new WOW(
+if (typeof WOW !== 'undefined') {
+  var wow = new WOW(
     {
       offset: 0,
       mobile: true,
     }
   )
   wow.init();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var serviceDelayMessage = document.querySelector(".service-delay-message");
+  var serviceDelayEnabled = serviceDelayMessage && serviceDelayMessage.dataset.avisoActivo !== "false";
+  var hasLoginError = new URLSearchParams(window.location.search).get("errorcode") === "3";
+
+  if (!serviceDelayMessage || !serviceDelayEnabled || hasLoginError || typeof bootstrap === "undefined") {
+    return;
+  }
+
+  var serviceDelayModal = new bootstrap.Modal(serviceDelayMessage);
+
+  serviceDelayModal.show();
+});
   
